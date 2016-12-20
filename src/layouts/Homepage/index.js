be44764941cf488f5c418cfc54aad0c90d1d5a87
Muhammad Muhajir <muhajirframe@gmail.com>
2016-12-20 21:06:59 +0700

    HomePage now use CenterVertical

--- src/layouts/Homepage/index.js
+++ src/layouts/Homepage/index.js
@@ -2,20 +2,21 @@ import React, {PropTypes} from "react"
 
 import BasicPage from '../BasicPage'
 import Button from '../../components/Button'
+import CenterVertical from '../../components/CenterVertical'
 
 import styles from './index.css'
 
 const Homepage = (props) => {
   return (
     <BasicPage { ...props }>
-      <div className={styles.container}>
+      <CenterVertical>
         <h1 className={styles.heading}>{props.head.title}</h1>
         <h2 className={styles.subheading}>{props.head.tagline}</h2>
         <div>
           <Button key={'one'} className={styles.button}>Test</Button>
           <Button key={'two'} className={styles.button}>Test</Button>
         </div>
-      </div>
+      </CenterVertical>
     </BasicPage>
   )
 }
