5a50cdc03c2ab23f93f251c92def092b2e8d3b9a
Muhammad Muhajir <muhajirframe@gmail.com>
2016-12-21 21:37:28 +0700

    add ContactForm

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,10 +1,15 @@
 import React, {PropTypes} from 'react';
+import styles from './index.css'
 
 const ContactForm = props => {
     return (
         <form>
-            <fieldset>
-                <input type={'text'} name={'name'} />
+            <fieldset className={styles.fieldset}>
+                <input type={'text'} name={'name'} className={styles.input} 
+                placeholder={'Your Name'}/>
+                <input type={'email'} name={'email'} className={styles.input} 
+                placeholder={'Your Email'}/>
+                <textarea placeholder={'Leave message here'} className={styles.textarea}/>
             </fieldset>
         </form>
     );
