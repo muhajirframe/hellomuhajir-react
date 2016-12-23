c5d9fb6b888c435715099836b55bf79c4571d3a7
Muhammad Muhajir <muhajirframe@gmail.com>
2016-12-23 19:47:55 +0700

    restyle Post

--- src/layouts/Post/index.js
+++ src/layouts/Post/index.js
@@ -1,7 +1,9 @@
 import React, { PropTypes } from "react"
 
 import LatestPosts from "../../components/LatestPosts"
-import Page from "../Page"
+import Loading from "../../components/Loading"
+import BasicPage from "../BasicPage"
+import {BodyContainer} from "phenomic"
 
 import styles from "./index.css"
 
@@ -10,29 +12,33 @@ const Post = (props) => {
   const pageDate = props.head.date ? new Date(props.head.date) : null
 
   return (
-    <Page
-      { ...props }
-      header={
-        <div>
-          <header className={ styles.header }>
-            {
-              pageDate &&
-              <time key={ pageDate.toISOString() }>
-                { pageDate.toDateString() }
-              </time>
-            }
-          </header>
-        </div>
-      }
-    >
+    <BasicPage { ...props } >
+      <header className={ styles.header }>
+      <h1 className={styles.title}>{props.head.title}</h1>
+        {
+          pageDate &&
+          <time key={ pageDate.toISOString() }>
+            { pageDate.toDateString() }
+          </time>
+        }
+      </header>
+      <div className={styles.body}>
+          {
+          props.isLoading
+          ? <Loading />
+          : <BodyContainer>{ props.body }</BodyContainer>
+          }
+      </div>
       <hr />
       <LatestPosts />
-    </Page>
+    </BasicPage>
   )
 }
 
 Post.propTypes = {
   head: PropTypes.object.isRequired,
+  body: PropTypes.string,
+  isLoading: PropTypes.bool,
 }
 
 export default Post
