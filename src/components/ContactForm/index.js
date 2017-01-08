e6a3d27968788fc6c5f2795154a9b8816d263326
Muhammad Muhajir <muhajirframe@gmail.com>
2017-01-08 18:24:54 +0700

    integrate with formspree and add submit button

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -3,13 +3,14 @@ import styles from './index.css'
 
 const ContactForm = () => {
     return (
-        <form>
+        <form action="https://formspree.io/muhajir@hellomuhajir.com" method="POST">
             <fieldset className={styles.fieldset}>
                 <input type={'text'} name={'name'} className={styles.input} 
                 placeholder={'Your Name'}/>
-                <input type={'email'} name={'email'} className={styles.input} 
+                <input type={'email'} name={'_replyto'} className={styles.input} 
                 placeholder={'Your Email'}/>
                 <textarea placeholder={'Leave message here'} className={styles.textarea}/>
+                <input type="submit" value="Send" className={styles.submit}/>
             </fieldset>
         </form>
     );
