4e8b50d790602d317f23fc14cd893bf5ae135a96
Muhammad Muhajir <muhajirframe@gmail.com>
2017-01-08 17:17:46 +0700

    add cta button to homepage

--- src/layouts/Homepage/index.js
+++ src/layouts/Homepage/index.js
@@ -1,18 +1,23 @@
-import React, {PropTypes} from "react"
+import React, { PropTypes } from "react"
 
 import BasicPage from '../BasicPage'
 import Button from '../../components/Button'
+import { Link } from 'phenomic'
 
 import styles from './index.css'
 
 const Homepage = (props) => {
+  let Buttons = props.head.cta.map((item) => (
+    <Link to={item.link} style={{'text-decoration':'none'}}>
+      <Button key={item.label} className={styles.button}>{item.label}</Button>
+    </Link>
+  ))
   return (
     <BasicPage { ...props }>
       <h1 className={styles.heading}>{props.head.title}</h1>
       <h2 className={styles.subheading}>{props.head.tagline}</h2>
       <div>
-        <Button key={'one'} className={styles.button}>Test</Button>
-        <Button key={'two'} className={styles.button}>Test</Button>
+        {Buttons}
       </div>
     </BasicPage>
   )
