4176fe098919dd7e2c6e924b3bf984352d2880f8
Muhammad Muhajir <muhajirframe@gmail.com>
2017-01-09 21:29:15 +0700

    add link to email's footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,11 +2,13 @@ import React, {PropTypes} from "react"
 
 import styles from "./index.css"
 
-const Footer = (props, { metadata : { pkg } }) => (
-  <footer className={ styles.footer }>
-    <a>{pkg.email}</a>
-  </footer>
-)
+const Footer = (props, { metadata : { pkg } }) => {
+  return (
+    <footer className={ styles.footer }>
+      <a href={`mailto:${ pkg.email }`}>{pkg.email}</a>
+    </footer>
+  )
+}
 
 Footer.contextTypes = {
   metadata : PropTypes.object.isRequired,
